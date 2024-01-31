8cdb1c06652fae8a9119e4f3f6ff5d5c23b90668
Kiran Jonnalagadda <kiran@hasgeek.com>
2024-01-31 17:36:21 +0530

    Bump min version to Python 3.9

--- setup.py
+++ setup.py
@@ -42,11 +42,10 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
